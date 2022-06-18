4e925ba828c7080a87ec386f918d3f2697d2e931
w568w <1278297578@qq.com>
2022-06-18 10:32:39 +0800

    feat: impl /courses

--- src/api/curriculum_board.rs
+++ src/api/curriculum_board.rs
@@ -1,4 +1,6 @@
-use actix_web::{get, post,HttpResponse, Responder};
+use actix_web::{get, post, HttpResponse, Responder, web};
+use sea_orm::{DatabaseConnection, EntityTrait};
+use crate::entity::coursegroup as CourseGroup;
 
 #[get("/")]
 pub async fn hello() -> impl Responder {
@@ -6,6 +8,11 @@ pub async fn hello() -> impl Responder {
 }
 
 #[get("/courses")]
-pub async fn get_course_groups() -> impl Responder {
-    HttpResponse::ServiceUnavailable().body("Poo!")
+pub async fn get_course_groups(db: web::Data<DatabaseConnection>) -> impl Responder {
+    let result: Result<Vec<serde_json::Value>, sea_orm::DbErr> =
+        CourseGroup::Entity::find().into_json().all(db.get_ref()).await;
+    match result {
+        Ok(groups) => HttpResponse::Ok().json(groups),
+        Err(e) => HttpResponse::InternalServerError().body(e.to_string())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ mod entity;
 
 use api::curriculum_board;
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-
+use sea_orm::{Database, DatabaseConnection};
+use crate::entity::coursegroup as CourseGroup;
+use sea_orm::EntityTrait;
 
 fn config(cfg: &mut web::ServiceConfig) {
     cfg.service(curriculum_board::hello)
@@ -12,9 +14,11 @@ fn config(cfg: &mut web::ServiceConfig) {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    let db: DatabaseConnection = Database::connect("mysql://root:root@127.0.0.1:3306/board").await.unwrap();
+    HttpServer::new(move || {
         App::new()
             .configure(config)
+            .app_data(web::Data::new(db.clone()))
     })
         .bind(("127.0.0.1", 8080))?
         .run()
